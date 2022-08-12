353434def228a92a98b04e86bcdc5f8a71005e5d
AntonBrito <tonybrito85@gmail.com>
2022-08-11 23:48:37 -0400

    Implemented a new style of navigation menu

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,4 +1,8 @@
 <nav>
-	<a href="/">Home</a>
-	<a href="/about">About</a>
+	<div class="bg-gray-600 ">
+		<div class="justify-center text-center p-4">
+			<a href="/" class="text-white text-2xl font-bold p-2 "> My Blogs</a>
+			<a href="/" class="text-white text-2xl font-bold p-2 "> New+</a>
+		</div>
+	</div>
 </nav>
