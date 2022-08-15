0f9a48414d16242a37d1e90ced142f8b99463d04
AntonBrito <tonybrito85@gmail.com>
2022-08-14 22:16:32 -0400

    Implemented about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,5 +1,37 @@
 <svelte:head>
-	<title>About Tech Blog</title>
+	<title>About ABLog</title>
 </svelte:head>
-<h1>About</h1>
-<p>This Website was built using SvelteKit, Things might change depending when you see this page</p>
+<div class="bg-gray-300 px-20 py-40">
+	<div class="grid grid-cols-2 flex-col md:grid-cols-1 lg:grid-cols-2">
+		<h3 class="flex text-4xl font-bold p-2">Hi, my name is</h3>
+		<br />
+		<h1 class="flex text-6xl font-bold p-2">Antonio Brito</h1>
+	</div>
+	<br />
+	<p class="flex text-2xl font-bold p-2">
+		I'm a Software Developer|Engineer father of two beautiful daughter, the creator of this web blog
+		application and it was built using SvelteKit, one of my favorite technology right now. SvelteKit
+		is realy hot and very new that Things might change depending when you see this page
+	</p>
+</div>
+<div class="bg-gray-600 px-20 py-40">
+	<div class="grid grid-cols-2 flex-col md:grid-cols-1 lg:grid-cols-2">
+		<h3 class="flex text-blue-300 text-4xl font-bold p-2">My Skills</h3>
+		<br />
+	</div>
+</div>
+<div class="bg-gray-300 px-20 py-40">
+	<div class="grid grid-cols-2 flex-col md:grid-cols-1 lg:grid-cols-2">
+		<h3 class="flex text-blue-300 text-4xl font-bold p-2">My Skills</h3>
+		<br />
+	</div>
+</div>
+<div class="bg-gray-600 px-20 py-40">
+	<div class="grid grid-cols-2 flex-col md:grid-cols-1 lg:grid-cols-2">
+		<h3 class="flex text-blue-300 text-4xl font-bold p-2 justify-center items-center">
+			Get in Touch
+		</h3>
+		<br />
+		<div />
+	</div>
+</div>
